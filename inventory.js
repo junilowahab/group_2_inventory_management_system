29db814cd56e49d7785c61d7229916ed979f1590
junilowahab <junilojoshuawahab@gmail.com>
2025-04-06 02:22:29 +0100

    Implemented deleting the product from the display and product from the invetory database when the delete button is clicked

--- inventory.js
+++ inventory.js
@@ -16,6 +16,34 @@ function addInventoryDetailsToDatabase(e){
         this.stock = stock;
     }
 
-    inventoryDatabase[inventoryProduct] = new productDatabase(inventoryProduct, inventoryPrice, inventoryStock);
-    console.log(inventoryDatabase);
+    if(inventoryProduct && inventoryPrice && inventoryStock){
+        inventoryDatabase[inventoryProduct] = new productDatabase(inventoryProduct, inventoryPrice, inventoryStock);
+    }else{
+        alert('Make sure all data is filled.');
+        console.log(inventoryDatabase)
+        return
+    }
+
+    let productDisplay = document.createElement('div');
+    productDisplay.setAttribute('class', 'product-display')
+    productDisplay.setAttribute('id', `${inventoryProduct}`)
+
+    let display = document.querySelector('.display');
+    display.appendChild(productDisplay);
+
+    productDisplay.innerHTML = `
+        <h4>${inventoryProduct}</h4>
+        <p>&#8358 ${inventoryPrice}</p>
+        <p>${inventoryStock} units</p>
+        <button class="delete ${productDisplay.id}">Delete</button>
+    `;
+
+    let deleteButtons = productDisplay.querySelectorAll('.delete');
+    deleteButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener('click', () => {
+            deleteButton.parentElement.remove();
+            delete inventoryDatabase[deleteButton.parentElement.id]
+        })
+    })
+    
 }
